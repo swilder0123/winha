cb08b6dfdc6b376aafcf05587167641bc8af232e
swilder <swilder@microsoft.com>
2020-05-27 07:03:19 -0700

    establishing commit

--- doextend.sh
+++
@@ -1,8 +0,0 @@
-az vm extension set \
-    -g winha-rg  \
-    --vm-name winha-node-0 \
-    --publisher Microsoft.Compute \
-    --name JsonADDomainExtension \
-    --version 1.3 \
-    --settings ./settings.json \
-    --protected-settings ./protected_settings.json
